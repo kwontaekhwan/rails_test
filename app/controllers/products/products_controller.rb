80838132e0ce9ab4fee93a195ca3a0503c78a66c
taekani <taekani@stunitas.com>
2019-05-22 23:48:53 +0900

    first commit

--- app/controllers/products/products_controller.rb
+++ app/controllers/products/products_controller.rb
@@ -4,9 +4,5 @@ def index
    format.html
    format.xls
    end
-  end
-  
-  config/initializers/mime_types.rb
-  Mime::Type.register "application/xls", :xls
-  views/products/index.xls.erb
+end
   
